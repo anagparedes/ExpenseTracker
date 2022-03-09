6d3e017d4188da300e5ced8393112ca48606df6f
anagparedes <anagparedesg@gmail.com>
2022-03-09 19:30:59 -0400

    [MOV] data from child to parent component

--- src/App.js
+++ src/App.js
@@ -26,9 +26,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense =>{
+    console.log('In App.js');
+    console.log(expense);
+  }
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expense data={expenses}/>
     </div>
   );
